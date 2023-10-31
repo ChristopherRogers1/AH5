4cc56beeb9dc72d76ef05d1adabca8c130ae3d40
ChristopherRogers1 <60368736+ChristopherRogers1@users.noreply.github.com>
2023-10-31 14:14:44 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,12 +1,33 @@
-class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
-    }
-  
+
+
+public class Solution {
     public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+
+        System.out.println(mostFrequentChar("eleventennine"));
+
     }
 
-  }
+    public static char mostFrequentChar(String input)
+    {
+        int highestCount=0, currentCount=0;
+        char highestChar = input.charAt(0), currentChar;
+        for (int i = 0; i < input.length(); i++)
+        {
+            currentChar = input.charAt(i);
+            for (int n = 0; n < input.length(); n++)
+            {
+                if (currentChar == input.charAt(n))
+                {
+                    currentCount++;
+                }
+            }
+            if (currentCount > highestCount)
+            {
+                highestCount = currentCount;
+                highestChar = currentChar;
+            }
+            currentCount=0;
+        }
+        return highestChar;
+    }
+}
